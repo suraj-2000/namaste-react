0c6a1bacdcab87c52fc063665efdc78916b7c01d
Suraj Singh <suraj_bt18@iiitkalyani.ac.in>
2025-10-17 19:03:07 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,6 @@
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm in h1 tag!"), React.createElement("h2", {}, "I'm in h2 tag!")]));
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root.render(parent);
+root2.render(heading);
